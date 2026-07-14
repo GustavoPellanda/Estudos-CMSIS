db6d6f8be296871be5e6adc9176536ce4444f093
GustavoPellanda <gusfipe@gmail.com>
2026-07-13 21:57:13 -0300

    Preparando inicialização.

--- Direct-Memory-Access/src/main.c
+++ Direct-Memory-Access/src/main.c
@@ -1,8 +1,25 @@
-#include <stdint.h>
+#include "stm32f103xb.h"
+#include "dma.h"
+#include "system.h"
 
-int main(void)
-{
-    while (1)
+uint8_t mensagem[] = "Teste DMA USART1\r\n";
+
+int main(void){
+
+    system_init();
+
+    dma_start_transfer(
+        DMA1,
+        DMA1_Channel4,
+        4,
+        mensagem,
+        &USART1->DR,
+        sizeof(mensagem)
+    );
+
+
+    while(1)
     {
+
     }
 }
